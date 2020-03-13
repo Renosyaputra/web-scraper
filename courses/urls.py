154d16aafb13ea850785d5ad4a1df76f94dbd9e8
renosytr <renosyaputra0001@gmail.com>
2020-03-13 20:35:13 +0530

    update searching function

--- courses/urls.py
+++ courses/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('search', views.new_search, name='new_search'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'base.html')
+    return render(request, 'courses/index.html')
+
+def new_search(request):
+    keyword = request.POST.get('keyword')
+    search_keyword = {
+        'keyword' : keyword,
+    }
+    return render(request, 'courses/new_search.html', search_keyword)
